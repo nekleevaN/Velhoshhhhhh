563edd6d24a81353a9cf74d9c437e124b5236b25
nekleevaN <nekleeva@gmail.com>
2023-02-23 16:09:51 +0200

    added gamno cede

---
+++ lab_1/ConsoleApplication1/LogCon.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef LogCon
+#define LogCon
+
+bool NOT(bool a1);
+bool AND(bool a1, bool b1);
+bool OR(bool a1, bool b1);
+bool IMP(bool a1, bool b1);
+bool EQU(bool a1, bool b1);
+bool XOR(bool a1, bool b1);
+bool func4(bool a1, bool b1, bool c1);
+
+#endif
